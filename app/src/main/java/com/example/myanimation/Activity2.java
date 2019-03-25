1ef218adf11a5bb98ef3b108f0eb7697848453e8
Udara Abeythilake <udara.i@eyepax.com>
2019-03-25 11:53:16 +0530

    #myanimation

--- app/src/main/java/com/example/myanimation/Activity2.java
+++ app/src/main/java/com/example/myanimation/Activity2.java
@@ -1,6 +1,8 @@
 package com.example.myanimation;
 
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -14,6 +16,21 @@ import java.util.Arrays;
 public class Activity2 extends AppCompatActivity {
 
 
+    private final int NR_OF_SIMULTANEOUS_SOUNDS = 7;
+    private final float LEFT_VOLUME = 1.0f;
+    private final float RIGHT_VOLUME = 1.0f;
+    private final int NO_LOOP = 0;
+    private final int PRIORITY = 0;
+    private final float NORMAL_PLAY_RATE = 1.0f;
+
+    private int mCSoundId;
+    private int mDSoundId;
+    private int mESoundId;
+    private int mFSoundId;
+    private int mGSoundId;
+    private int mASoundId;
+    private int mBSoundId;
+    private SoundPool mSoundPool;
 char[] notearr={};
 String noteName;
     @Override
@@ -21,6 +38,17 @@ String noteName;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
 
+        mSoundPool=new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS, AudioManager.STREAM_MUSIC,0);
+
+
+        // TODO: Load and get the IDs to identify the sounds
+        mCSoundId= mSoundPool.load(getApplicationContext(), R.raw.note1_c,1);
+        mDSoundId= mSoundPool.load(getApplicationContext(), R.raw.note2_d,1);
+        mESoundId= mSoundPool.load(getApplicationContext(), R.raw.note3_e,1);
+        mFSoundId= mSoundPool.load(getApplicationContext(), R.raw.note4_f,1);
+        mGSoundId= mSoundPool.load(getApplicationContext(), R.raw.note5_g,1);
+        mASoundId= mSoundPool.load(getApplicationContext(), R.raw.note6_a,1);
+        mBSoundId= mSoundPool.load(getApplicationContext(), R.raw.note7_b,1);
     }
 
 //    public void openActivity3(View view) {
@@ -41,12 +69,12 @@ String noteName;
         EditText notes=(EditText)findViewById(R.id.editText);
 
        String name= notes.getText().toString();
-        Log.d("Finished",name);
+        Log.d("Xylaphone",name);
 
         switch (id){
 
             case R.id.btnA:
-                Log.d("Finished","A");
+                Log.d("Xylaphone","A");
                 noteName=name+"A";
                 notes.setText(noteName, TextView.BufferType.EDITABLE);
 
@@ -54,37 +82,41 @@ String noteName;
 
 
             case R.id.btnB:
-                Log.d("Finished","B");
+                Log.d("Xylaphone","B");
                 noteName=name+"B";
                 notes.setText(noteName, TextView.BufferType.EDITABLE);
                 break;
             case R.id.btnC:
-                Log.d("Finished","C");
+                Log.d("Xylaphone","C");
                 noteName=name+"C";
                 notes.setText(noteName, TextView.BufferType.EDITABLE);
                 break;
 
             case R.id.btnD:
-                Log.d("Finished","D");
+                Log.d("Xylaphone","D");
                 noteName=name+"D";
                 notes.setText(noteName, TextView.BufferType.EDITABLE);
                 break;
             case R.id.btnE:
-                Log.d("Finished","E");
+                Log.d("Xylaphone","E");
                 noteName=name+"E";
                 notes.setText(noteName, TextView.BufferType.EDITABLE);
                 break;
             case R.id.btnF:
-                Log.d("Finished","F");
+                Log.d("Xylaphone","F");
                 noteName=name+"F";
                 notes.setText(noteName, TextView.BufferType.EDITABLE);
                 break;
             case R.id.btnG:
-                Log.d("Finished","G");
+                Log.d("Xylaphone","G");
                 noteName=name+"G";
                 notes.setText(noteName, TextView.BufferType.EDITABLE);
                 break;
 
+            case R.id.btnNewLine:
+                Log.d("Xylaphone","new line");
+                noteName=name+"\n";
+                notes.setText(noteName, TextView.BufferType.EDITABLE);
 
 
         }
@@ -94,14 +126,60 @@ String noteName;
     public void Play(View v){
 
         notearr=noteName.toCharArray();
-        Log.d("Finished", Arrays.toString(notearr));
+       //Log.d("Xylaphone", Arrays.toString(notearr));
 
         String singlenote;
         //super.finish();
+
+
         for (int i=0;i<notearr.length;i++){
-            
+
+
+            String legth=Integer.toString(notearr[i]);
+            Log.d("Xylaphone",legth);
+            try {
+                PlaySound(legth);
+                Thread.sleep(500);
+            }
+           catch (InterruptedException e){
+               Log.d("Xylaphone","error");
+           }
         }
         //overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+
+    public  void PlaySound(String ascii){
+        switch (ascii){
+            case "65":
+                Log.d("Xylaphone","Play 65");
+                mSoundPool.play(mASoundId,LEFT_VOLUME, RIGHT_VOLUME,PRIORITY, NO_LOOP, NORMAL_PLAY_RATE  );
+                break;
+            case "66":
+                Log.d("Xylaphone","Play 66");
+                mSoundPool.play(mBSoundId,LEFT_VOLUME, RIGHT_VOLUME,PRIORITY, NO_LOOP, NORMAL_PLAY_RATE  );
+                break;
+            case "67":
+                Log.d("Xylaphone","Play 67");
+                mSoundPool.play(mCSoundId,LEFT_VOLUME, RIGHT_VOLUME,PRIORITY, NO_LOOP, NORMAL_PLAY_RATE  );
+                break;
+            case "68":
+                Log.d("Xylaphone","Play 68");
+                mSoundPool.play(mDSoundId,LEFT_VOLUME, RIGHT_VOLUME,PRIORITY, NO_LOOP, NORMAL_PLAY_RATE  );
+                break;
+            case "69":
+                Log.d("Xylaphone","Play 69");
+                mSoundPool.play(mESoundId,LEFT_VOLUME, RIGHT_VOLUME,PRIORITY, NO_LOOP, NORMAL_PLAY_RATE  );
+                break;
+            case "70":
+                Log.d("Xylaphone","Play 70");
+                mSoundPool.play(mFSoundId,LEFT_VOLUME, RIGHT_VOLUME,PRIORITY, NO_LOOP, NORMAL_PLAY_RATE  );
+                break;
+            case "71":
+                Log.d("Xylaphone","Play 71");
+                mSoundPool.play(mGSoundId,LEFT_VOLUME, RIGHT_VOLUME,PRIORITY, NO_LOOP, NORMAL_PLAY_RATE  );
+                break;
+
+        }
     }
 
 
--- app/src/main/java/com/example/myanimation/MainActivity.java
+++ app/src/main/java/com/example/myanimation/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.myanimation;
 
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
@@ -30,17 +32,71 @@ import android.content.pm.PackageManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int NR_OF_SIMULTANEOUS_SOUNDS = 7;
+    private final float LEFT_VOLUME = 1.0f;
+    private final float RIGHT_VOLUME = 1.0f;
+    private final int NO_LOOP = 0;
+    private final int PRIORITY = 0;
+    private final float NORMAL_PLAY_RATE = 1.0f;
 
-
+    private int mCSoundId;
+    private int mDSoundId;
+    private int mESoundId;
+    private int mFSoundId;
+    private int mGSoundId;
+    private int mASoundId;
+    private int mBSoundId;
+    private SoundPool mSoundPool;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // TODO: Create a new SoundPool
+        mSoundPool=new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS, AudioManager.STREAM_MUSIC,0);
+
+
+        // TODO: Load and get the IDs to identify the sounds
+        mCSoundId= mSoundPool.load(getApplicationContext(), R.raw.note1_c,1);
+        mDSoundId= mSoundPool.load(getApplicationContext(), R.raw.note2_d,1);
+        mESoundId= mSoundPool.load(getApplicationContext(), R.raw.note3_e,1);
+        mFSoundId= mSoundPool.load(getApplicationContext(), R.raw.note4_f,1);
+        mGSoundId= mSoundPool.load(getApplicationContext(), R.raw.note5_g,1);
+        mASoundId= mSoundPool.load(getApplicationContext(), R.raw.note6_a,1);
+        mBSoundId= mSoundPool.load(getApplicationContext(), R.raw.note7_b,1);
 
 
 
+    }
+
+    public void playC(View v){
+        Log.d("Xylaphone","played");
+        mSoundPool.play(mCSoundId,LEFT_VOLUME, RIGHT_VOLUME,PRIORITY, NO_LOOP, NORMAL_PLAY_RATE  );
+    }
+    public void playD(View v){
+        Log.d("Xylaphone","played");
+        mSoundPool.play(mDSoundId,LEFT_VOLUME, RIGHT_VOLUME,PRIORITY, NO_LOOP, NORMAL_PLAY_RATE  );
+    }
+    public void playE(View v){
+        Log.d("Xylaphone","played");
+        mSoundPool.play(mESoundId,LEFT_VOLUME, RIGHT_VOLUME,PRIORITY, NO_LOOP, NORMAL_PLAY_RATE  );
+    }
+    public void playF(View v){
+        Log.d("Xylaphone","played");
+        mSoundPool.play(mFSoundId,LEFT_VOLUME, RIGHT_VOLUME,PRIORITY, NO_LOOP, NORMAL_PLAY_RATE  );
+    }
+    public void playG(View v){
+        Log.d("Xylaphone","played");
+        mSoundPool.play(mGSoundId,LEFT_VOLUME, RIGHT_VOLUME,PRIORITY, NO_LOOP, NORMAL_PLAY_RATE  );
+    }
+    public void playA(View v){
+        Log.d("Xylaphone","played");
+        mSoundPool.play(mASoundId,LEFT_VOLUME, RIGHT_VOLUME,PRIORITY, NO_LOOP, NORMAL_PLAY_RATE  );
+    }
+    public void playB(View v){
+        Log.d("Xylaphone","played");
+        mSoundPool.play(mBSoundId,LEFT_VOLUME, RIGHT_VOLUME,PRIORITY, NO_LOOP, NORMAL_PLAY_RATE  );
     }
 
 
